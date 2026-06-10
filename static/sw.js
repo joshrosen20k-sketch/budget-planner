5d860a952063c1bd4a34de60c14a1f5f4b787e28
Josh Rosen <joshrosen@Joshs-MacBook-Air.local>
2026-06-10 12:24:02 -0500

    Fix service worker caching

--- static/sw.js
+++ static/sw.js
@@ -1,18 +1,17 @@
-const CACHE = "budget-v3";
-const ASSETS = ["/", "/static/style.css"];
+const CACHE = "budget-v4";
 
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener("activate", e => {
-  e.waitUntil(clients.claim());
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    ).then(() => clients.claim())
+  );
 });
 
 self.addEventListener("fetch", e => {
-  if (e.request.method !== "GET") return;
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
